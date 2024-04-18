1de994796ff01ce936e12b19d4d9b410239ab003
Joost-Wim Boekesteijn <joost-wim@boekesteijn.nl>
2024-04-18 10:47:28 +0200

    remove old analytics trackers

--- src/ga.js
+++
@@ -1,5 +0,0 @@
-// Global site tag (gtag.js) - Google Analytics
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-145445183-2');
